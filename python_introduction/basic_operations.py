bcbe0a4d484f881143a70b5ccb6a9c376f3742ae
johnitopaisah <johnitopaisah@gmail.com>
2024-06-18 13:35:24 +0200

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+number1 = 10
+number2 = 5
+print("Sum of", number1, "and", number2, "is", (number1 + number2))
+print('Subtraction of', number1, 'and', number2, 'is', (number1 - number2))
+print('Multiplication of', number1, 'and', number2, 'is', (number1*number2))
