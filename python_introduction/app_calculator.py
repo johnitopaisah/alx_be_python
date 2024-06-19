b51a09c1239e67148e95b503d09fcc571bdf951b
johnitopaisah <johnitopaisah@gmail.com>
2024-06-19 23:06:25 +0200

    Dog Class examples

--- python_introduction/app_calculator.py
+++ python_introduction/app_calculator.py
@@ -1,3 +1,10 @@
-first_number = float(input("First: "))
-second_number = int(input("Second: "))
-print(f"Sum: {first_number + second_number}")
+# print(f"Sum: {float(input("First: ")) + float(input("Second: "))}")
+
+my_name = "John Itopa Isah"
+print(my_name.upper())
+print(my_name.replace('o', 'i', 1))
+print(my_name.count('o'))
+print(my_name.isalpha('o'))
+
+is_back = 3 > 2
+is_back.
