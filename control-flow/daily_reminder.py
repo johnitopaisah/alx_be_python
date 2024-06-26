5cdfbf7c77181e7358267a8a21c3452f9b873265
johnitopaisah <johnitopaisah@gmail.com>
2024-06-26 10:44:29 +0200

    daily_task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 task = input("Enter your task: ")
-reminder = input("Priority (high/medium/low): ")
-time_bound = input("Is it time-bound? (yes/no): ")
+priority = input("Priority (high/medium/low): ")
+reminder = input("Is it time-bound? (yes/no): ")
 
-match reminder:
-    case 'high':
-        if time_bound == "yes":
-            print(f"{task} is a {reminder} priority task that requires immediate attention today!")
-        else:
-            print(f"{task} is a {reminder} priority task that requires immediate attention today!")
+match priority:
+    case 'high' if reminder == 'yes':
+        print(f"{task} is a {priority} priority task that requires immediate attention today!")
+        # else:
+        #     print(f"{task} is a {priority} priority task that requires immediate attention today!")
     case 'medium':
         if time_bound == 'yes':
-            print(f"{task} is a {reminder} priority task, but try to do this on time!")
+            print(f"{task} is a {priority} priority task, but try to do this on time!")
         else:
-            print(f"{task} is a {reminder} priority task consider completing it when you have free time")
+            print(f"{task} is a {priority} priority task consider completing it when you have free time")
     case 'low':
         if time_bound == 'yes':
-            print(f"{task} is a {reminder} priority task, but try to do this on time!")
+            print(f"{task} is a {priority} priority task, but try to do this on time!")
         else:
-            print(f"{task} is a {reminder} priority task consider completing it when you have free time")
+            print(f"{task} is a {priority} priority task consider completing it when you have free time")
