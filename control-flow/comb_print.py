adfef12acec0edfcd9e6226db6129f62c354d59f
johnitopaisah <johnitopaisah@gmail.com>
2024-06-26 20:16:03 +0200

    to print value from 0 -> 99

--- control-flow/comb_print.py
+++ control-flow/comb_print.py
@@ -2,11 +2,12 @@
 
 # Want to print value from 0 -> 99 and all the number
 # printed must be 2-digits
+# use 2 print() ==> How
 
 for num in range(0, 100):
     if num < 10:
         print(f'0{num}', end=', ')
-    elif num == 99:
-        print(num)
+    # elif num == 99:
+    #     print(num)
     else:
-        print(num, end=', ')
+        print(num, end=', ' if num != 99 else '\n')
