987298b085ec0c812c92197e4f1ea7347a2ea9b8
johnitopaisah <johnitopaisah@gmail.com>
2024-06-26 18:39:33 +0200

    match value

---
+++ control-flow/print.py
@@ -0,0 +1,8 @@
+string_1 = "john"
+string_2 = "isah"
+madlib = """On a beautiful f'{string_1}' day, I went to the zoo.\
+            I saw a funny (adjective) monkey swinging from the trees. \
+            Then, I spotted a majestic (adjective) lion lounging in the sun.\ 
+            What a wild and (adjective) experience!"""
+
+print(madlib)
