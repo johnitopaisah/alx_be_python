f15d724ea1022b3e0a4903f24fc40170a4bee35b
johnitopaisah <johnitopaisah@gmail.com>
2024-06-30 22:49:32 +0200

    update

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
+
 number = int(input("Enter a number to see its multiplication table: "))
-for j in range(1, 11):
-    # print(f"The multiplication table for {j} from (1 -> 10)")
-    for i in range(1, number+1):
-        print(f"{j} * {i} = {j * i}", end='\t')
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
+ 
