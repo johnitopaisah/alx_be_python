5eaceb6519a872aa6273dcbd8a24c4b67f6503ef
johnitopaisah <johnitopaisah@gmail.com>
2024-08-01 11:37:53 +0200

    committed all changes

--- base/decorator.py
+++ base/decorator.py
@@ -1,4 +1,26 @@
-def compose(f, g, x):
-    return f(g(x))
+# def compose(f, g, x):
+#     return f(g(x))
 
-compose(print, len, "Hello, world!")
+# compose(print, len, "Hello, world!")
+
+
+import random
+
+def random_power():
+    def f(x):
+        return x ** 2
+    
+    def g(x):
+        return x ** 3
+    
+    def h(x):
+        return x ** 4
+    
+    functions = [f, g, h]
+    return random.choice(functions)
+
+results = []
+for i in range(10):
+    p = random_power()
+    results.append(p(3))
+print(results)
